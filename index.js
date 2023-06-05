a5936edda05b035397491401dbaf329b3e174cc9
luis_Alberto <109326400+AlbertoBonifaz@users.noreply.github.com>
2023-06-04 19:10:18 -0500

    Adicion del logo y mejoramiento de la funcionlidad del desencriptado

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function encriptar() {
 
 
     let textoCifrado = texto    
-        .replace(/a/gi, "ai")
-        .replace(/e/gi, "enter")
-        .replace(/i/gi, "imes")
-        .replace(/o/gi, "ober")
-        .replace(/u/gi, "ufat");
+    .replace(/a/gi, "ai")
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
 
     if(texto.length != 0){
         document.getElementById("texto").value = textoCifrado;
@@ -23,4 +23,31 @@ function encriptar() {
         parrafo.textContent ="Ingresa texto que deseas encriptar o desencriptar";
         alert("Debes ingresar algun texto");
     }  
+}
+
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+    if(texto.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent ="";
+        muñeco.src = "./img/desencriptado.jpg";
+    } else {
+        muñeco.src = "./img/muñeco.png";
+        tituloMensaje.textContent ="Ningun mensaje encontrado";
+        parrafo.textContent ="Ingresa texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algun texto");
+    }
 }
